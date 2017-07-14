8eac2575be9f2c4ba6860abf7162e6ca3898a074
AuNguyenTrungNguyen <nguyen06111996@gmail.com>
2017-07-14 16:53:47 +0700

    Chon Loai Tai Khoan

---
+++ app/src/main/java/android/familydoctor/Fragment/FragmentBacSi.java
@@ -0,0 +1,27 @@
+package android.familydoctor.Fragment;
+
+import android.familydoctor.R;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Au Nguyen on 7/14/2017.
+ */
+
+public class FragmentBacSi extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.doctor, container, false);
+        return rootView;
+    }
+}
