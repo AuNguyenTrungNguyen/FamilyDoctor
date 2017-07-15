d0e1a6d6e0d3538fa87c7aba53dd27cb949239ae
quocb14005xx <quocb14005xx@gmail.com>
2017-07-15 12:13:13 +0700

    map tab 3

--- app/src/main/java/android/familydoctor/Fragment/DanhSachBacSi_BenhNhan.java
+++ app/src/main/java/android/familydoctor/Fragment/DanhSachBacSi_BenhNhan.java
@@ -1,44 +1,63 @@
 package android.familydoctor.Fragment;
 
+import android.content.pm.PackageManager;
 import android.familydoctor.R;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class DanhSachBacSi_BenhNhan extends Fragment {
 
-
-    private DatabaseReference databaseReference;
+    MapView mMapView;
+    private GoogleMap googleMap;
+    //private DatabaseReference databaseReference;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab3_danhasch_bs_bn, container, false);
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        databaseReference= FirebaseDatabase.getInstance().getReference();
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+                if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+                }
+                googleMap.setMyLocationEnabled(true);
+                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
 
-        even();
-
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("HBQ").snippet("huynh bao quoc"));
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18));
+            }
+        });
         return rootView;
     }
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
-    public void even(){
-
-    }
-
-
-
-
-
 }
